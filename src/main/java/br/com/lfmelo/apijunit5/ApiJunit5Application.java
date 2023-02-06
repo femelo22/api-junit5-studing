0965557c3dcecc907074b3e39c026784a78cd4bb
Luiz Fernando <103144451+LuizSpeeder@users.noreply.github.com>
2023-02-06 17:15:18 -0300

    feat: começando projeto

---
+++ src/main/java/br/com/lfmelo/apijunit5/ApiJunit5Application.java
@@ -0,0 +1,13 @@
+package br.com.lfmelo.apijunit5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJunit5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiJunit5Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lfmelo/apijunit5/ApiJunit5ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lfmelo.apijunit5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJunit5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
